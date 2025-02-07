67fee05f7a83d5766646619e181b102f979c7e1b
Alex Bertotto <ajb5206@gmail.com>
2025-02-07 10:40:10 -0800

    set up test for signup route, set named route in routes, update link on home.html.erb and add title to new user page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/home", to: "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
