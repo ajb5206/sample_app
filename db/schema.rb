438c99053dd801e9271c3bb8d78b66398c397341
Alex Bertotto <ajb5206@gmail.com>
2025-02-13 18:12:05 -0800

    add has_secure_passowrd to user model and create password_digest column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_13_235559) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_14_021057) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
