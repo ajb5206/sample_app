eaab08cc67d0eebbd71c640d08f83a1722afcddb
Alex Bertotto <ajb5206@gmail.com>
2025-02-18 16:46:17 -0800

    addtest for error messages

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
-    assert_no_difference "User.count" do
+    assert_no_difference "User.count" do  # assert no difference between User.count before and after post request if invalid
       post users_path, params: { user: { name: "",
                                          email: "user@invalid",
                                          password: "foo",
@@ -11,5 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_response :unprocessable_entity
     assert_template "users/new"
+    assert_select "div#<CSS id for error explanation"
+    assert_select "div.<CSS class for field with error"
   end
 end
