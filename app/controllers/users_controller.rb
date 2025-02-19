03b83497f1893d43366d7941a84055f88c4d0bdf
Alex Bertotto <ajb5206@gmail.com>
2025-02-18 16:55:58 -0800

    if successful user creation, redirect to newly created user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # send to chosen view
+      redirect_to @user
     else
       render "new", status: :unprocessable_entity
     end
